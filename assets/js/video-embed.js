0a763ecb1849f7d0048f299c8be79206db9dc0fb
Guillem Anguera <ganguera@gmail.com>
2020-05-24 21:35:02 +0200

    move to jquery lazy

--- assets/js/video-embed.js
+++ assets/js/video-embed.js
@@ -1,3 +1,6 @@
 $(".portfolio-modal").on('hidden.bs.modal', function (e) {
   $(this).find("iframe").attr("src", $(this).find("iframe").attr("src"));
 });
+$(function(){
+  $("iframe[data-src]").Lazy();
+});
