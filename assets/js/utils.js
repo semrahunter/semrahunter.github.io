b92f42cb0ef529a0d7887d41101fe9d00fbdf9da
Guillem Anguera <ganguera@gmail.com>
2020-05-27 23:27:49 +0200

    performance fixes

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,15 +1,11 @@
-// Lazy Load
 $(function(){
     $('.lazy').lazy();
-
 });
+
 $(".portfolio-modal").on('show.bs.modal', function (e) {
-    $("img[data-src]").Lazy();
-    $("iframe[data-src]").Lazy();
-    $('.img-fluid').lazy();
+    $(".lazy-modal").Lazy();
 });
 
-//Stop YouTube on close modal
 $(".portfolio-modal").on('hide.bs.modal', function (e) {
     $(this).find("iframe").attr("src", $(this).find("iframe").attr("src"));
 });
