4c26abd0dc987deaae1a34a81928b2732ab80e83
Guillem Anguera <ganguera@gmail.com>
2020-06-18 00:29:16 +0200

    404 Error as a section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -35,22 +35,24 @@
         $(".navbar-collapse").collapse("hide");
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 74,
-    });
+    if (top.location.pathname === '/') {
+        // Activate scrollspy to add active class to navbar items on scroll
+        $("body").scrollspy({
+            target: "#mainNav",
+            offset: 74,
+        });
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+        // Collapse Navbar
+        var navbarCollapse = function () {
+            if ($("#mainNav").offset().top > 100) {
+                $("#mainNav").addClass("navbar-shrink");
+            } else {
+                $("#mainNav").removeClass("navbar-shrink");
+            }
+        };
+        // Collapse now if page is not at top
+        navbarCollapse();
+        // Collapse the navbar when page is scrolled
+        $(window).scroll(navbarCollapse);
+    }
 })(jQuery); // End of use strict
